a81591869fd814c4647eae1231dc07ea38a03976
sudhanhq <sudhanhq@gmail.com>
2014-10-16 13:44:30 -0600

    new changes

---
+++ Assets/Bullets.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var bullet:Transform;
+function Start () {
+
+}
+
+function Update () {
+
+
+	if (Input.GetMouseButtonDown(0)){
+
+	var shoot = Instantiate(bullet, transform.position, transform.rotation);
+    shoot.rigidbody.AddForce(shoot.transform.forward * 500.0);
+	//clone.velocity = transform.TransformDirection (Vector3.forward * 10);
+	}
+	
+	}
