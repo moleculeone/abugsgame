00f7f5e70ebe6cf415f51053e08b7607d8d363f0
Hitesh <hitesharmaa@gmail.com>
2014-11-06 15:15:19 -0700

    Added ray-casting solution with sudhanshu but the player is flickering

---
+++ Assets/FireRayCast.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireRayCast : MonoBehaviour {
+	public GameObject bulletObject;
+	public GameObject aimCursor;
+	public Transform fireFrom;
+	public float shotSpeed = 20.0f;
+	
+	void Update () {
+		RaycastHit rhInfo;
+		Vector3 shootToward;
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		if(Physics.Raycast(mouseRay, out rhInfo, 150.0f)) {
+			shootToward = rhInfo.point;
+		} else {
+			shootToward = mouseRay.origin + mouseRay.direction * 50.0f;
+		}
+		if(aimCursor != null) {
+			aimCursor.transform.position = shootToward;
+		}
+		transform.LookAt(shootToward);
+		
+		if( Input.GetMouseButtonDown(0) )
+		{
+			GameObject shotGO = (GameObject)Instantiate(bulletObject, fireFrom.position, fireFrom.rotation);
+			Vector3 deltaPos = shootToward - shotGO.transform.position;
+			shotGO.rigidbody.velocity = deltaPos.normalized * shotSpeed;
+		}
+	}
+}
