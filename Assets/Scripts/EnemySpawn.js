95dfd11baf64c3ead373aefcc2867e41ab5316ab
reag27@hotmail.com <reag27@hotmail.com>
2014-11-07 19:30:17 -0700

    missing scripts pickupstones and enemyspawn

---
+++ Assets/Scripts/EnemySpawn.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+var enemy :  Transform;
+var spawnlimit= 5;
+private var CurEnemies=0;
+var waitforsec:int= 10;
+
+function Start () {
+	
+	
+		InvokeRepeating("spawn", 10, waitforsec);
+}
+
+function Update () {
+
+	
+	
+}
+
+function spawn()
+{
+	if (CurEnemies< spawnlimit)
+		{
+		var pos =transform.position;
+		pos= Vector3(transform.position.x,1,transform.position.z);
+		var enemy = Instantiate(enemy,pos, transform.rotation );
+	
+		CurEnemies+=1;
+		//enemy.rigidbody.AddForce(enemy.transform.forward * 1.0);
+	
+		}
+}
