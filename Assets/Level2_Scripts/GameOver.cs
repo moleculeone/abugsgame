1a027341687a36dfcb7dfa7ed66f8468bf46f499
Hitesh <hitesharmaa@gmail.com>
2014-11-24 08:58:42 -0700

    Added Level 2 and renamed level3a to level 3.

---
+++ Assets/Level2_Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter () {
+		
+		Application.LoadLevel("NightScene");
+		
+	}
+
+	
+	}
+
