92643da83d8016c2662bafbd615ba029312b179b
reag27@hotmail.com <reag27@hotmail.com>
2014-12-02 16:26:28 -0700

    untracked files

---
+++ Assets/PlayerMove.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+var walkSpeed = 2.0;
+var TurnSpeed = 10.0;
+var directiony;
+var directionx;
+private var rota:float;
+private var trans:float;
+function Start () {
+
+}
+
+function Update () {
+
+			animation.Play("idle");
+			rota  = Input.GetAxis("Horizontal")* TurnSpeed;
+			trans = Input.GetAxis("Vertical");
+			
+      		if (Input.GetAxisRaw("Horizontal"))
+      		{
+      		        	
+   				transform.Rotate(0, rota,0);
+      		}
+      		if (Input.GetAxisRaw("Vertical"))
+      		{
+      			animation.CrossFade("walk");
+      			if (trans == 1)      			
+      				transform.Translate (Vector3.forward * Time.deltaTime*walkSpeed);
+      			else if (trans == -1)      
+      				transform.Translate (Vector3.back * Time.deltaTime*walkSpeed);
+      		}
+}
