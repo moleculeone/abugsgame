1f730a8819a1a153694fe557ef3bed025ac1cc81
Hitesh <hitesharmaa@gmail.com>
2014-12-04 19:01:17 -0700

    Obstacle for level3 and some other changes

---
+++ Assets/PuzzleLevel3.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var obText1: Text;
+var obText2: Text;
+var bridge: GameObject;
+var ob1: GameObject;
+var ob2: GameObject;
+//static var showPanel:boolean=false;
+
+
+public var blocked : AudioClip;
+
+function Start () {
+}
+
+function Update () {
+
+
+}
+
+function OnTriggerEnter (info : Collider) 
+    {
+    		audio.clip = blocked;
+    		audio.Play();
+		
+    	if (info.tag=="tool2")
+    	{
+    	bridge.transform.position.y = (-0.23);
+    	ob2.SetActive(false);
+    	//GameMaster.objectNumber = 2;
+    	//toolText2.enabled = true;
+    	yield WaitForSeconds(3);
+    	//toolText2.enabled = false;
+    	
+    	}
+    	
+   }
