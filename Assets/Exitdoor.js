a2d8dcd11f0ad2709b5b226804e8266c4f7aafb8
reag27@hotmail.com <reag27@hotmail.com>
2014-12-06 18:02:49 -0700

    -fix random door script and groped items.

---
+++ Assets/Exitdoor.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function OnTriggerEnter (info : Collider) 
+{
+	if (info.name=="Player")
+	{
+
+		 if (transform.tag.EndsWith(RandomDoor.doornumber)==true)
+		 {
+	
+			Application.LoadLevel("NightScene");
+		
+		}
+	
+	}
+
+
+}
